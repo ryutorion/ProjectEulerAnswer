76ba001dfb66f4766a777bc527a95c6ec06e7da3
Masahiro Kimoto <masahiro.kimoto@gmail.com>
2013-01-01 23:58:48 +0900

    Another answer of Problem 1

--- cpp/0001.cpp
+++ cpp/0001.cpp
@@ -2,12 +2,13 @@
 
 int main()
 {
-    int result = 3;
-    for(int i = 4; i < 1000; ++i){
-        if(i % 3 == 0 || i % 5 == 0){
-            result += i;
-        }
-    }
+    int arithmeticSeriesOfThree   = (999 /  3) * ( 3 + 999) / 2;
+    int arithmeticSeriesOfFive    = (999 /  5) * ( 5 + 995) / 2;
+    int arithmeticSeriesOfFifteen = (999 / 15) * (15 + 990) / 2;
 
-    std::cout << result << std::endl;
+    std::cout <<
+        arithmeticSeriesOfThree +
+        arithmeticSeriesOfFive  -
+        arithmeticSeriesOfFifteen
+    << std::endl;
 }
